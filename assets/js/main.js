d0569da7f7c3a60acc0c29cae7b95c6ce52803e1
Куценко Валерий <valekutce@gmail.com>
2021-11-12 16:58:48 +0200

    Мобильное меню в футере сайта

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,13 @@ $(document).ready(function (){
         dots: true,
         autoplaySpeed: 5000,
     });
+
+    $('.burg').click(function (){
+        $('footer').toggleClass('footer-nav__active');
+    });
+    $('.burg').click(function (){
+        $(this).toggleClass('burg__active');
+        $('.footer-text h2').toggleClass("footer-text__active");
+    });
+   
 });
